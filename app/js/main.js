86cd8438241884c9077e49b095b581cea74bf6b2
pr9n1k <kiril.pr9n1k@gmail.com>
2021-06-13 22:46:21 +0300

    оставшиеся блоки, десктоп 90%

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,31 @@ const swiper = new Swiper('.swiper-container', {
     },
     loop: true
   });
+  const swiperSecond = new Swiper('.swiper-content__second', {
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: 'auto', //кол-во показываемых слайдов
+    watchOverflow: true, //если слайдов мало, - слайдер отключается
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true
+  });
+
+//плавная прокрутка
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+//плавная прокрутка конец
